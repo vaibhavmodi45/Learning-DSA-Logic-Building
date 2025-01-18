56a908ba492ba171703304db470271551f0db13f
Vaibhav Modi <modivaibhav817@gmail.com>
2025-01-18 23:43:29 +0530

    Add initial C++ programs for learning basic concepts and variable usage

---
+++ Practice CPP Concepts/variablesusing.cpp
@@ -0,0 +1,30 @@
+//This program let us know to implement and initialize the variables and how they can be used to simplify the code.
+
+/* Here we are learning following things like:
+
+#1 How to define and initialize variables.
+#2 How to use them in our code.
+#3 How to use datatypes to define different kind of data within our program.
+#4 How to define single line and multi line comments.
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 5;    // This show the variable a which will store the interger value that is defined as 5.
+    float pi = 3.14;  //This line of code shows that the variable pi will store the floating point value which is 3.14.
+    char myName = 'v';   //This line of code shows that the variable myName store the character value which is v.
+    bool eligible = false;   //This line of code shows that the variable eligible stores the boolean value that is false.
+
+    cout << a << endl;
+    cout << pi << endl;
+    cout << myName << endl;
+    cout << eligible << endl;
+    return 0;
+}
+
+
+// We can use the 
