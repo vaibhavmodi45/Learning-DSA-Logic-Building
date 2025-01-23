317844563c0e15dc95f143eb7decbb85a404d93a
Vaibhav Modi <modivaibhav817@gmail.com>
2025-01-24 00:03:54 +0530

    Add output for double variable in variablesusing.cpp

--- Practice CPP Concepts/variablesusing.cpp
+++ Practice CPP Concepts/variablesusing.cpp
@@ -20,6 +20,7 @@ int main() {
     bool eligible = false;   //This line of code shows that the variable eligible stores the boolean value that is false.
     double price = 10.3333;   //This line of code shows that the variable eligible stores the boolean value that is false.
     cout << a << endl;
+    
     cout << pi << endl;
     cout << myName << endl;
     cout << eligible << endl;
