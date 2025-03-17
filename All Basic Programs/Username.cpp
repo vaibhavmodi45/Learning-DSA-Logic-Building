328a19994e5661fe3738de599e52324c01fecdef
Vaibhav Modi <modivaibhav817@gmail.com>
2025-03-17 23:05:52 +0530

    Today a date 17 March writted some basic programs and executed them

---
+++ All Basic Programs/Username.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "Enter your name: " << endl;
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
+    return 0;
+}
---
+++ All Basic Programs/getuserinput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "You entered: " << a << endl;
+    return 0;
+}
