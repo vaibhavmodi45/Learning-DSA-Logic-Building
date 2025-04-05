3f04cb7c83663feadaf045e1252d8be52a0e3337
Vaibhav Modi <modivaibhav817@gmail.com>
2025-04-05 20:13:04 +0530

    Enhance swapping of numbers program to include user input and output results

--- All Basic Programs/swapingofNumber.cpp
+++ All Basic Programs/swapingofNumber.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main() {
-    int
+    int a, b, temp;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "After swapping, the numbers are: " << a << " and " << b << endl;
+    return 0;
 }
