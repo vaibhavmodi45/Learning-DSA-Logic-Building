16057e06267e01074e42d988e713f7cfdb360e3c
Vaibhav Modi <modivaibhav817@gmail.com>
2025-04-07 23:43:36 +0530

    Swaping Problem

--- All Basic Programs/swapingofNumber.cpp
+++ All Basic Programs/swapingofNumber.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+// This program swaps two numbers using a temporary variable
+int swapusingthird() {
     int a, b, temp;
     cout << "Enter two numbers: ";
     cin >> a >> b;
@@ -10,4 +11,21 @@ int main() {
     b = temp;
     cout << "After swapping, the numbers are: " << a << " and " << b << endl;
     return 0;
+}
+
+// This program swaps two numbers without using a temporary variable
+int swapwithoutusingthird() {
+    int a,b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "After swapping, the numbers are: " << a << " and " << b << endl;
+    return 0;
+}
+
+int main() {
+    // swapusingthird();
+    swapwithoutusingthird();
 }
