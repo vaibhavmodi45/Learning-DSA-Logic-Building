f2b979db12a19677a0eece7d382eb1f74942c1bd
Vaibhav Modi <modivaibhav817@gmail.com>
2025-04-10 23:59:34 +0530

    a

---
+++ C++ Programming Practice/Basic C++ Programs/swapNumbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, temp;
+    cout << "Enter Values of a and b: ";
+    cin >> a >> b;
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
