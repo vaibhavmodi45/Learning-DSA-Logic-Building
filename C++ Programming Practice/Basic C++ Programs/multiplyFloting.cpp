0ee7b869accb14c5380bf28a49be34d288108b17
Vaibhav Modi <modivaibhav817@gmail.com>
2025-04-12 19:05:35 +0530

    floating point numbers multiplication

---
+++ C++ Programming Practice/Basic C++ Programs/multiplyFloting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    float r = 5.0; // radius of the circle
+    float pi = 3.14; // value of pi
+    float area; // variable to store the area of the circle
+
+    area = pi * r * r; // calculate the area of the circle
+
+    cout << "Area of the circle: " << area << endl; // output the area
+    return 0; // return 0
+}
