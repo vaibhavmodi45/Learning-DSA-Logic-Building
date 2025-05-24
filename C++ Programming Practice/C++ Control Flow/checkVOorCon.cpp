6d781ead8384959d79dc126707c71b7c220a5d1a
Vaibhav Modi <modivaibhav817@gmail.com>
2025-05-24 20:15:48 +0530

    cf

---
+++ C++ Programming Practice/C++ Control Flow/checkVOorCon.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter a character = ";
+    cin >> ch;
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ) {
+        cout << "Vowel";
+    }
+    else {
+        cout << "Consonant";
+    }
+    return 0;
+}
