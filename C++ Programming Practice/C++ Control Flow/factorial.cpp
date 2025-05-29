968e96b5b9ebafedfb1f5d4b2f225c35ea8e7b0f
Vaibhav Modi <modivaibhav817@gmail.com>
2025-05-29 11:22:45 +0530

    cf

--- C++ Programming Practice/C++ Control Flow/factorial.cpp
+++ C++ Programming Practice/C++ Control Flow/factorial.cpp
@@ -1,1 +1,15 @@
-G
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number to calculate factorial = ";
+    cin >> n;
+
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+    cout << fact;
+    return 0;
+}
