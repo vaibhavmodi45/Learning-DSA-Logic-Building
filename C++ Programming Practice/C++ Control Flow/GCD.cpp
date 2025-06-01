3ab697c35ce942766454d8da25deb0b20f426cc6
Vaibhav Modi <modivaibhav817@gmail.com>
2025-06-01 13:41:47 +0530

    cf

---
+++ C++ Programming Practice/C++ Control Flow/GCD.cpp
@@ -0,0 +1,21 @@
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout <<
+// }
+
+// C++ program to find gcd of two numbers using 
+// inbuilt __gcd() and gdc() function
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  	int a = 24, b = 16;
+
+	// Finding gcd of a and b using __gcd()
+  	cout << __gcd(a, b) << endl;
+  
+	return 0;
+}
