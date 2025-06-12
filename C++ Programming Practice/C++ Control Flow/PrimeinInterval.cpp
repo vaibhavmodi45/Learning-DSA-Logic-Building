9c65af74ebbb6e409fab17a4640f28c03118d84b
Vaibhav Modi <modivaibhav817@gmail.com>
2025-06-12 20:49:41 +0530

    cf

--- C++ Programming Practice/C++ Control Flow/PrimeinInterval.cpp
+++ C++ Programming Practice/C++ Control Flow/PrimeinInterval.cpp
@@ -3,27 +3,22 @@ using namespace std;
 
 int main() {
     int a, b;
-    cout << "Enter both the values of range";
+    cout << "Enter both the values of range = ";
     cin >> a >> b;
-    int cnt = 0;
 
     for(int i = a; i <= b; i++){
 
-        if( a <= 1 ){
-            cout << "Not a Prime";
-        }
+        if( i <= 1 ) continue;
 
+        int cnt = 0;
         for(int j = 1; j <= i; j++) {
-            if(a % j == 0) {
+            if(i % j == 0) {
                 cnt++;
             }
         }
 
-        if( cnt > 2) {
-            for
-        }
-        else {
-            cout << "It's a Prime";
+        if( cnt == 2) {
+            cout << i << " ";
         }
     }
     return 0;
