286d6782ab50ce23c27059641568417cd2516511
Vaibhav Modi <modivaibhav817@gmail.com>
2025-06-25 23:59:46 +0530

    cf

--- C++ Programming Practice/C++ Control Flow/power.cpp
+++ C++ Programming Practice/C++ Control Flow/power.cpp
@@ -6,6 +6,10 @@ int main() {
     cout << "Enter the base and power values for calculations = ";
     cin >> base >> exp;
 
+    if(exp == 0) {
+        cout << "1";
+    }
+
     for(int i=0; i < exp; i++ ) {
         int power = base * base;
         cout << "power of " << base << " is = " << power << endl;
